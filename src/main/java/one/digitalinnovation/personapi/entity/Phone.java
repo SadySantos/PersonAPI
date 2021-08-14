204f6c930d4870215ab3e5022f496b2a04442f56
SadySantos <sadysantos@hotmail.com.br>
2021-08-13 21:09:44 -0300

    Initial commit

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -0,0 +1,44 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.*;
+import one.digitalinnovation.personapi.enums.PhoneType;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Builder
+@AllArgsConstructor
+//@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Phone phone = (Phone) o;
+
+        return Objects.equals(id, phone.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 745010098;
+    }
+}
---
+++ src/test/java/one/digitalinnovation/personapi/service/PersonServiceTest.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.personapi.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonServiceTest {
+
+}
